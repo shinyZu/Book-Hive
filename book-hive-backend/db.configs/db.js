e93bd000c35bb108c0631c71b05ae42f7199f946
shinyZu <shinysirichandra1141@gmail.com>
2024-11-28 19:38:11 +0530

    connected backend with mongodb atlas

---
+++ book-hive-backend/db.configs/db.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+// Database URL to connect with
+const url = process.env.URL;
+
+// Create MongoDB Connection
+const establishConnection = mongoose.connect(url, {
+  useNewUrlParser: true,
+});
+
+const conn = mongoose.connection;
+
+// runs everytime when connected to mongodb
+conn.on("open", () => {
+  console.log("Connected to MongoDB Atlas...!");
+});
+
+export { establishConnection, conn };
--- book-hive-backend/server.js
+++ book-hive-backend/server.js
@@ -1,12 +1,26 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import { establishConnection } from "./db.configs/db.js";
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4001;
+
+// Invoke the method to establish connection with mongoDB
+establishConnection;
+
+const baseURL = "/book-hive/api/";
 
 app.get('/', (req, res) => {
     res.send('Hello, Express!');
 });
 
+app.get(`${baseURL}/`, (req, res) => {
+    console.log(req);
+    res.send("<h1>Hello Express!!!</h1>");
+});
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Express App listening on Port ${PORT}`);
 });
